6df2f9552da8ce1ad1342468f76ca2b6ec2ba105
Jacques <jacques.furst@tno.nl>
2025-03-20 15:21:47 +0100

    Revert "Initial commit"
    This reverts commit ca8978c4b3fd77d9815dadf17919aa73ed0efcaa.

--- ollama_model/llama32.py
+++
@@ -1,9 +0,0 @@
-from langchain_ollama import ChatOllama
-
-#TODO: figure out whether Ollama is truly the best way to do this... 
-
-def load_llm():
-    model = ChatOllama(
-        model="llama3.2",
-        language="en")
-    return model
