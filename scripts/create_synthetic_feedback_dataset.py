eea28e973c82faca7cdfa149e0f9271f34e3d60c
Jacques Fürst <jacques.furst@tno.nl>
2025-05-26 16:15:41 +0200

    code for createing dataet to get synthetic feedback is there

--- scripts/create_synthetic_feedback_dataset.py
+++ scripts/create_synthetic_feedback_dataset.py
@@ -2,8 +2,23 @@ import json
 
 json_files_first_doc = ['/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/model_answers/model_answers_acts_Participatiewet_most_recent_public.json',
                         '/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/model_answers/acts_rijksbegroting_model_answers.json',
+                        '/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/model_answers/acts_besluit_Vw_model_answers.json',
+                        '/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/model_answers/model_answers_facts_rijksbegrotingscyclus.json',
+                        '/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/model_answers/facts_besluit_Vw_model_answers.json'
               ]
 
-json_files_2nd_doc = [  '/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/model_answers/acts_besluit_Vw_model_answers.json',
-                        '/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/model_answers/model_answers_facts_rijksbegrotingscyclus.json',
-              ]
+merged_data = []
+
+# Load and merge all JSON data
+for file in json_files_first_doc:
+    with open(file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        if isinstance(data, list):
+            merged_data.extend(data)
+        else:
+            merged_data.append(data)  # If it's a dict or single object
+
+
+# Optionally, write to first data file
+with open('/home/jacques.furst/development/RAG/flintfiller-precondition-rl/data/participant_data/synthetic_feedback_dataset.json', 'w', encoding='utf-8') as f:
+    json.dump(merged_data, f, ensure_ascii=False, indent=2)
